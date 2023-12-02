eac695810bcddd5dd947c81b7b3f9e27d7fb9140
yueyingh <133840831+yueyingh@users.noreply.github.com>
2023-12-02 16:53:56 -0500

    Update genlasso_admm.cpp

--- augmentedADMM/src/genlasso_admm.cpp
+++ augmentedADMM/src/genlasso_admm.cpp
@@ -83,8 +83,97 @@ Rcpp::List admm_genlasso(const arma::mat &A, const arma::colvec &b,
   return (output);
 }
 
+
 // [[Rcpp::export]]
-Rcpp::List admm_genlasso_aug(const arma::mat &A, const arma::colvec &b,
+Rcpp::List admm_genlasso_for_graph(const arma::mat &A, const arma::colvec &b,
+                                   const arma::mat &D, const arma::mat &C, 
+                                   const double lambda1, const double lambda2, 
+                                   const double reltol, const double rho
+                                   const double abstol, const int maxiter) {
+  // 1. get parameters
+  const int n = A.n_rows;
+  const int p = A.n_cols;
+  const int m = D.n_rows - p;
+
+  // 2. set ready
+  arma::colvec x(p, fill::randn);
+  x /= 10.0;
+  arma::colvec z(D * x);
+  arma::colvec z1 = z.head(p); 
+  arma::colvec z2 = z.tail(m);
+  arma::colvec u(D * x - z);
+  arma::colvec q(p, fill::zeros);
+  arma::colvec zold(z);
+  arma::colvec x_hat(p, fill::zeros);
+
+  // 3. precompute static variables for x-update and factorization
+  arma::mat Atb = A.t() * b;
+  arma::mat U = genlasso_factor(A, rho, D.t() * D); // returns upper
+  arma::mat L = U.t();
+
+  // 4. iteration
+  arma::vec h_objval(maxiter, fill::zeros);
+  arma::vec h_r_norm(maxiter, fill::zeros);
+  arma::vec h_s_norm(maxiter, fill::zeros);
+  arma::vec h_eps_pri(maxiter, fill::zeros);
+  arma::vec h_eps_dual(maxiter, fill::zeros);
+
+  double sqrtn = std::sqrt(static_cast<float>(n));
+  int k;
+  for (k = 0; k < maxiter; k++) {
+    arma::colvec u1 = u.head(p);  
+    arma::colvec u2 = u.tail(m);
+    // 4-1. update 'x'
+    q = Atb + rho * D.t() * (z - u / rho); // temporary value
+    x = solve(trimatu(U), solve(trimatl(L), q));
+    //        if (m >= n){
+    //            x = solve(trimatu(U),solve(trimatl(L),q));
+    //        } else {
+    //            x = q/rho -
+    //            (A.t()*solve(trimatu(U),solve(trimatl(L),A*q)))/rho2;
+    //        }
+
+    // 4-2. update 'z'
+    zold = z;
+    z1 = genlasso_shrinkage(x + u1 / rho, lambda1 / rho);
+    z2 = genlasso_shrinkage(C * x + u2 / rho, lambda2 / rho);
+    z = join_cols(z1, z2);
+
+    // 4-3. update 'u'
+    u = u + rho * (D * x - z);
+
+    // 4-3. diagnostics, reporting
+    h_objval(k) = genlasso_objective_graph(A, b, C, lambda1, lambda2, x, z);
+    h_r_norm(k) = arma::norm(D * x - z);
+    h_s_norm(k) = arma::norm(-rho * D.t() * (z - zold));
+    if (norm(x) > norm(-z)) {
+      h_eps_pri(k) = sqrtn * abstol + reltol * norm(D * x);
+    } else {
+      h_eps_pri(k) = sqrtn * abstol + reltol * norm(-z);
+    }
+    h_eps_dual(k) = sqrtn * abstol + reltol * norm(D.t() * u);
+
+    // 4-4. termination
+    if ((h_r_norm(k) < h_eps_pri(k)) && (h_s_norm(k) < h_eps_dual(k))) {
+      break;
+    }
+  }
+
+  // 5. report results
+  List output;
+  output["x"] = x;             // coefficient function
+  output["objval"] = h_objval; // |x|_1
+  output["k"] = k;             // number of iterations
+  output["r_norm"] = h_r_norm;
+  output["s_norm"] = h_s_norm;
+  output["eps_pri"] = h_eps_pri;
+  output["eps_dual"] = h_eps_dual;
+  return (output);
+}
+
+
+// [[Rcpp::export]]
+Rcpp::List aug_admm_genlasso(const arma::mat &A, const arma::colvec &b,
                              const arma::mat &D, const arma::mat &M,
                              const double lambda, const double reltol,
                              const double abstol, const int maxiter,
@@ -167,7 +256,7 @@ Rcpp::List admm_genlasso_aug(const arma::mat &A, const arma::colvec &b,
 }
 
 // [[Rcpp::export]]
-Rcpp::List admm_genlasso_for_graph(const arma::mat &A, const arma::colvec &b,
+Rcpp::List aug_admm_genlasso_for_graph(const arma::mat &A, const arma::colvec &b,
                                    const arma::mat &D, const arma::mat &M,
                                    const arma::mat &C, const double lambda1,
                                    const double lambda2, const double reltol,
